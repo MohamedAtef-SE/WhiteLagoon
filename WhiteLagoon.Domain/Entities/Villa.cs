a30c31cb8dec64a6e0b638f7228889c2a9f7def5
mohamedatef-SE <mohamedatefyoussef@gmail.com>
2024-12-07 17:56:38 +0200

    Add project files.

---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,15 @@
+﻿namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageURL { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateData { get; set; }
+    }
+}
